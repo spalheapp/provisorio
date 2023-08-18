b9fc1426df6829441d62d67066c690083267e0a4
Marcos <71572572+spalheapp@users.noreply.github.com>
2023-08-17 21:56:32 -0300

    Add files via upload

--- scripts/whatsapp/alugue.js
+++ scripts/whatsapp/alugue.js
@@ -6,4 +6,4 @@ const quantidade = document.querySelector('#quantidade')
 const email = document.querySelector('#email')
 
 function simular() {
-    window.location.href = `https://api.whatsapp.com/send?phone=558199992438&text=Nome:%20${nome.value}%0AEndereço:%20${endereco.value}%0ATipo:%20${tipoDeImovel.value}%0AContato:%20${telefone.value}%0AQuantidade:%20${quantidade.value}%0AE-mail:%20${email.value}`}
+    window.location.href = `https://api.whatsapp.com/send?phone=5581981914968&text=Nome:%20${nome.value}%0AEndereço:%20${endereco.value}%0ATipo:%20${tipoDeImovel.value}%0AContato:%20${telefone.value}%0AQuantidade:%20${quantidade.value}%0AE-mail:%20${email.value}`}
--- scripts/whatsapp/index.js
+++ scripts/whatsapp/index.js
@@ -12,7 +12,7 @@ function reserva(){
     const entrada = dataEntrada.value.split("-").reverse().join().replace(/,/g, "/")
     const saida = dataSaida.value.split("-").reverse().join().replace(/,/g, "/")
 
-    window.location.href = `https://api.whatsapp.com/send?phone=558199992438&text=Local%20de%20reserva:%20${lugar.value}%0AData%20de%20check-in:%20${entrada}%0AData%20de%20checkout:%20${saida}%0AQuantidade%20de%20adultos:%20${adulto.value}%0AQuantidade%20de%20crian%C3%A7as:%20${crianca.value}`
+    window.location.href = `https://api.whatsapp.com/send?phone=5581981914968&text=Local%20de%20reserva:%20${lugar.value}%0AData%20de%20check-in:%20${entrada}%0AData%20de%20checkout:%20${saida}%0AQuantidade%20de%20adultos:%20${adulto.value}%0AQuantidade%20de%20crian%C3%A7as:%20${crianca.value}`
 }
 
 
@@ -24,5 +24,5 @@ const email = document.querySelector("#promocaoEmail")
 function promocao(){
     console.log('funciona')
     alert('funcionann')
-    window.location.href = `https://api.whatsapp.com/send?phone=558199992438&text=Nome:%20${nome.value}%0ATelefone:%20${telefone.value}%0AEmail:%20${email.value}`
+    window.location.href = `https://api.whatsapp.com/send?phone=5581981914968&text=Nome:%20${nome.value}%0ATelefone:%20${telefone.value}%0AEmail:%20${email.value}`
 }
